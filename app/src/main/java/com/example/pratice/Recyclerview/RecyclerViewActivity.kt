0dd125b0542d543fc7bc06baa7da380c3a282257
daeyeong1573 <57317043+daeyeong1573@users.noreply.github.com>
2021-03-07 18:38:57 +0900

    [CREATE] RecyclerView

---
+++ app/src/main/java/com/example/pratice/Recyclerview/RecyclerViewActivity.kt
@@ -0,0 +1,14 @@
+package com.example.pratice.Recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pratice.R
+
+class RecyclerViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+
+
+    }
+}
